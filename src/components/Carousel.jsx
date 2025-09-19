80b3d9eb2240ad2a84a55ffe32f77704b6b28217
Sakshi Wagh <sakshikwagh17@gmail.com>
2025-09-19 09:22:02 +0530

    Added side effect in carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import classNames from "classnames";
 import { Left, Right } from "neetoicons";
@@ -12,6 +12,12 @@ const Carousel = ({ imageUrls, title }) => {
 
   const handlePrevious = () =>
     setCurrentIndex((currentIndex - 1 + imageUrls.length) % imageUrls.length);
+
+  useEffect(() => {
+    const interval = setInterval(handleNext, 3000);
+
+    return () => clearInterval(interval);
+  });
 
   return (
     <div className="flex flex-col items-center">
